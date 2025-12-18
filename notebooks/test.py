9760ee67bd7216f7efbd201f5ba507e329633a84
shubhamkragrawal <shubhamagrawal12963@gmail.com>
2025-12-18 02:21:46 -0500

    updated files

--- notebooks/test.py
+++ notebooks/test.py
@@ -1,10 +1,13 @@
 import pandas as pd
 
-df = pd.read_csv('data/raw/online_retail.csv', encoding='ISO-8859-1')
+df = pd.read_csv('../results/experiment_results.csv')
 print(df.columns)
-# data/raw/online_retail.csv
-# cols = ['InvoiceNo', 'StockCode', 'Description', 'Quantity', 'InvoiceDate',
-#        'Price', 'CustomerID', 'Country']
-# df.columns = cols
-# df.rename(columns={'ï»¿InvoiceNo': 'InvoiceNo'}, inplace=True)
-# df.to_csv('data/raw/online_retail.csv', index=False)
+df_sorted = df.sort_values(by='f1_score', ascending=False)
+
+# 2. Extract metrics from the top row for columns 'c' and 'd'
+top_row = df_sorted.iloc[0]
+f1_score = top_row['f1_score']
+accuracy = top_row['accuracy']
+precision = top_row['precision']
+recall = top_row['recall']
+print(f"F1 Score: {f1_score}, Accuracy: {accuracy}, Precision: {precision}, Recall: {recall}")
